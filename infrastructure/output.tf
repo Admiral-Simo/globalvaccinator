1b6123b907f85f9cd0ba9bd90a2eebdf8904b15b
Admiral-Simo <admiralsimo16@gmail.com>
2026-02-24 17:14:22 +0000

    disable some things in the infra

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,10 +1,8 @@
 output "frontend_env_file" {
   description = "COPY THIS into your frontend/.env file (and GitHub Secret PROD_ENV_FILE)"
   value = <<EOT
-NEXT_PUBLIC_API_URL=${aws_apigatewayv2_api.https_proxy.api_endpoint}/api
 NEXT_PUBLIC_COGNITO_USER_POOL_ID=${aws_cognito_user_pool.globalvaccinator_pool.id}
 NEXT_PUBLIC_COGNITO_CLIENT_ID=${aws_cognito_user_pool_client.client_app.id}
 NEXT_PUBLIC_AWS_REGION=${var.aws_region}
-NEXT_PUBLIC_S3_BUCKET_NAME=${aws_s3_bucket.frontend.bucket}
 EOT
 }
